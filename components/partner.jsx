a35c35ac9daeb6e681b1c7d4ed590f3fa2a3c0e1
gabrieldlp-able <138610376+gabrieldlp-able@users.noreply.github.com>
2023-07-19 15:55:45 -0500

    Update partner.jsx
    Reformat all code ralated to the partner vs Partner letter case error.

--- components/partner.jsx
+++ components/partner.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Partner = ({name}) => {
+const Partner = ({ name }) => {
   return (
-    <div className='px-10 py-5'>
-        <div className='mx-10 rounded-full text-gray-600 shadow-md border-2 border-gray-600 hover:bg-gray-600 hover:text-white'>
-            <p className='px-5 py-5'>{name}</p>
-        </div>
+    <div className="px-10 py-5">
+      <div className="mx-10 rounded-full text-gray-600 shadow-md border-2 border-gray-600 hover:bg-gray-600 hover:text-white">
+        <p className="px-5 py-5">{name}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default partner
+export default Partner;
