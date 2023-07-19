0a8235c54b07992a048c9d1a47f6f8c4e176ab89
gabrieldlp-able <138610376+gabrieldlp-able@users.noreply.github.com>
2023-07-19 15:52:44 -0500

    Update components/partner.jsx
    Changed partner to Partner to keep correct case convention.
    
    Co-authored-by: Federico García <fede@divinodi.com>

--- components/partner.jsx
+++ components/partner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const partner = ({name}) => {
+const Partner = ({name}) => {
   return (
     <div className='px-10 py-5'>
         <div className='mx-10 rounded-full text-gray-600 shadow-md border-2 border-gray-600 hover:bg-gray-600 hover:text-white'>
